06a0ebbb1aec1289f5f1ccdce74342d08a991faf
mnedza <marcelnedza2000@gmail.com>
2024-11-19 13:26:33 +0100

    Created general basic AuthPage structure

--- client/src/MainSocialMediaApp/Auth/page/AuthPage.tsx
+++ client/src/MainSocialMediaApp/Auth/page/AuthPage.tsx
@@ -1,5 +1,30 @@
-const AuthPage = () => {
-  return <h1>AuthPage</h1>
+import { useState } from "react";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+
+interface AuthPageData {
+  isLoggedIn: boolean;
+}
+
+const AuthPage: React.FC = () => {
+  const [loginData, setLoginData] = useState<AuthPageData>({
+    isLoggedIn: false,
+  });
+
+  const authHandler = () => {
+    setLoginData((prevState) => ({
+      ...prevState,
+      isLoggedIn: !prevState.isLoggedIn,
+    }));
+  };
+
+  return (
+    <div>
+      {loginData.isLoggedIn && <SignIn />}
+      {!loginData.isLoggedIn && <SignUp />}
+      <button onClick={authHandler}>sign in / sign up</button>
+    </div>
+  );
 };
 
 export default AuthPage;
