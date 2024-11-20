529819785b68946a25625dfc45f90e081115419d
mnedza <marcelnedza2000@gmail.com>
2024-11-20 13:18:51 +0100

    I made simple structure of AuthForm when prefere when I want to use sign Up Fields or sign in Field and I give it to the form and create fields on that

---
+++ client/src/MainSocialMediaApp/Auth/components/AuthForm.tsx
@@ -0,0 +1,50 @@
+interface AuthFormProps {
+  hasAccount: boolean;
+  setHasAccount: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const signInFields = [
+  { name: "email", type: "email" },
+  { name: "password", type: "password" },
+];
+
+const signUpFields = [
+  { name: "username", type: "text" },
+  ...signInFields,
+  { name: "confirmPassword", type: "password" },
+];
+
+const AuthForm: React.FC<AuthFormProps> = ({ hasAccount, setHasAccount }) => {
+  const fields = hasAccount ? signInFields : signUpFields;
+
+  const swapHandler = () => {
+    setHasAccount((prevState) => !prevState);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form Submitted!");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {fields.map((field) => (
+          <div key={field.name}>
+            <label>{field.name}</label>
+            <input name={field.name} type={field.type} />
+          </div>
+        ))}
+        <div>
+          <span>Try to </span>
+          <button onClick={swapHandler} type="button">
+            {hasAccount ? "Sign In" : "Sign Up"}
+          </button>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default AuthForm;
--- client/src/MainSocialMediaApp/Auth/page/AuthPage.tsx
+++ client/src/MainSocialMediaApp/Auth/page/AuthPage.tsx
@@ -1,29 +1,12 @@
 import { useState } from "react";
-import SignIn from "../components/SignIn";
-import SignUp from "../components/SignUp";
-
-interface AuthPageData {
-  isLoggedIn: boolean;
-}
+import AuthForm from "../components/AuthForm";
 
 const AuthPage: React.FC = () => {
-  const [loginData, setLoginData] = useState<AuthPageData>({
-    isLoggedIn: false,
-  });
-
-  const authHandler = () => {
-    setLoginData((prevState) => ({
-      ...prevState,
-      isLoggedIn: !prevState.isLoggedIn,
-    }));
-  };
-
+  const [hasAccount, setHasAccount] = useState<boolean>(false);
   return (
-    <div>
-      {loginData.isLoggedIn && <SignIn />}
-      {!loginData.isLoggedIn && <SignUp />}
-      <button onClick={authHandler}>sign in / sign up</button>
-    </div>
+    <>
+      <AuthForm hasAccount={hasAccount} setHasAccount={setHasAccount} />
+    </>
   );
 };
 
